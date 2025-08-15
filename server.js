91e4d5e93b6498018b08bd7714a12a39dd31c6ce
Swaminathan PL <srinath0209@gmail.com>
2025-08-15 17:31:48 +0530

    added connection to db

--- server.js
+++ server.js
@@ -4,3 +4,13 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
 
+const connectDB = require("./config/db.js");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});
