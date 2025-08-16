35bd9bb2b50da70e645939a0eea847b21c47d383
Swaminathan PL <srinath0209@gmail.com>
2025-08-16 10:47:47 +0530

    changed the Base_URL from localhost

--- public/config/config.js
+++ public/config/config.js
@@ -1,7 +1,8 @@
 // API Configuration
 export const API_CONFIG = {
-  BASE_URL: "/api",
+  BASE_URL: "https://task-management-deployment.onrender.com/api",
   ENDPOINTS: {
+
     LOGIN: "/auth/login",
     SIGNUP: "/auth/signup",
     TASKS: "/tasks"
